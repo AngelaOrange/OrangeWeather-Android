e9bc79ff041cd53cd6624e5858dcc80be6bbbbd4
AngelaOrange <1850519949@qq.com>
2018-10-01 20:16:04 +0800

    init project

--- app/src/main/java/cn/edu/pku/zhangjiaju/orangeweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/zhangjiaju/orangeweather/MainActivity.java
@@ -1,6 +1,12 @@
 package cn.edu.pku.zhangjiaju.orangeweather;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
 }
